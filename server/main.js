1a4482f6cf2a8425f7cc4b8aae57cbb99ea1fbf0
Sarah <sarah.mattar@mac.com>
2019-07-01 14:43:10 -0700

    remove commented out code

--- server/main.js
+++ server/main.js
@@ -3,43 +3,4 @@ import { Meteor } from "meteor/meteor";
 
 Meteor.startup(() => {
 
-	// let user = {
-	// 	name: "Sarah",
-	// 	location: "Vancouver"
-	// }
-
-	// let person = {
-	// 	...user, 
-	// 	age: 32
-	// }
-
-	// console.log(person);
-
-	// let bike = "Bianchi"
-
-	// let stuff = {
-	// 	bike, laptop: "Apple", headphones: "Beats", car: "Mazda"
-	// }
-
-	// console.log(stuff);
-
-	// let house = {
-	// 	bedrooms: 4,
-	// 	bathrooms: 3,
-	// 	storeys: 2
-	// }
-
-	// let yearBuilt = 1993;
-
-	// let neighbourhood = {
-	// 	...house, 
-	// 	address: "116 Taurus Drive",
-	// 	subdivision: "Hanwell",
-	// 	yearBuilt
-	// }
-
-	
-	// console.log(neighbourhood);
-
-	
 });
